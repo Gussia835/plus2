fe5095515f1b2de7f3fda10980f348a479eca21f
Gussia835 <146509661+Gussia835@users.noreply.github.com>
2024-10-28 09:20:26 +0300

    Добавлено 1-е задание

--- ConsoleApplication1/ConsoleApplication1/Classwork.h
+++ ConsoleApplication1/ConsoleApplication1/Classwork.h
@@ -1,3 +1,37 @@
 #pragma once
 
 #include <iostream>
+
+class worker
+{
+private:
+	std::string _name;
+	int _salary;
+	std::string _job;
+
+public:
+	//конструктор
+	worker(std::string name, int salary, std::string job) : _name(name), _salary(salary), _job(job) {};
+
+	//функции-члены для доступа к полям
+	
+	//получение имени
+	std::string GetName()
+	{
+		return _name;
+	}
+
+	//получение зарплаты
+	int GetSalary()
+	{
+		return _salary;
+	}
+
+	//получение должности
+	std::string GetJob()
+	{
+		return _job;
+	}
+
+
+};
