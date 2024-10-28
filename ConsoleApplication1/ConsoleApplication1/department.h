3f89cfedd43f9055ec748a5cfd99deb9014ddcc3
Gussia835 <146509661+Gussia835@users.noreply.github.com>
2024-10-28 09:29:20 +0300

    Добавлено 2-е задание

---
+++ ConsoleApplication1/ConsoleApplication1/department.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Classwork.h"
+#include <vector>
+
+class department
+{
+private:
+	std::string _Company_name;
+	std::vector<worker> _Employees;
+
+public:
+	department(std::string compny_name, std::vector<worker> employees) : _Company_name(compny_name), _Employees(employees) {};
+
+	std::string GetNameCompany()
+	{
+		return _Company_name;
+	}
+
+	std::vector<worker> GetEmployees()
+	{
+		return _Employees;
+	}
+
+	void PrintEmployees()
+	{
+		for (worker w : _Employees)
+		{
+			std::cout << "Имя работника: " << w.GetName() << "\nЗарплата: " << w.GetSalary() << "\nДолжность: " << w.GetJob() << 
+															std::endl << std::endl;
+
+		}
+	}
+
+
+};
